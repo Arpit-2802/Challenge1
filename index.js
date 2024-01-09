d44f9e62a31b889d16b1f94424a127f00ae7ea1a
Arpit-2802 <arpitthakur2802@gmail.com>
2024-01-09 15:19:23 +0530

    Final

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const box = document.getElementById('mybox');
 const startButton = document.getElementById('start-button');
 
-let boxPosition = { x: 0, y: 0 };
+let boxP = { x: 0, y: 0 };
 
 function startGame() {
   var element=document.getElementById('mybox')
   element.className='box';
-  boxPosition = {
+  boxP = {
     x: (window.innerWidth - box.offsetWidth) / 2,
     y: (window.innerHeight - box.offsetHeight) / 2
   };
@@ -17,16 +17,16 @@ function startGame() {
 function handleKeyPress(event) {
   switch (event.key) {
     case 'ArrowUp':
-      boxPosition.y -= 10;
+      boxP.y -= 10;
       break;
     case 'ArrowDown':
-      boxPosition.y += 10;
+      boxP.y += 10;
       break;
     case 'ArrowLeft':
-      boxPosition.x -= 10;
+      boxP.x -= 10;
       break;
     case 'ArrowRight':
-      boxPosition.x += 10;
+      boxP.x += 10;
       break;
   }
 
@@ -35,10 +35,8 @@ function handleKeyPress(event) {
 
 function updateBoxPosition() {
   
-  boxPosition.x = Math.max(0, Math.min(window.innerWidth - box.offsetWidth, boxPosition.x));
-  boxPosition.y = Math.max(0, Math.min(window.innerHeight - box.offsetHeight, boxPosition.y));
-
-  // Update box position
-  box.style.left = `${boxPosition.x}px`;
-  box.style.top = `${boxPosition.y}px`;
+  boxP.x = Math.max(0, Math.min(window.innerWidth - box.offsetWidth, boxP.x));
+  boxP.y = Math.max(0, Math.min(window.innerHeight - box.offsetHeight, boxP.y));
+  box.style.left = `${boxP.x}px`;
+  box.style.top = `${boxP.y}px`;
 }
